d3227dfc60c60de6b7c94455def6afd27b3a020f
cinek5 <marcinkrawczyk97@hotmail.com>
2018-03-29 18:13:16 +0200

    refactor

--- app/src/main/java/com/example/cinek/bmi/BMI.java
+++ app/src/main/java/com/example/cinek/bmi/BMI.java
@@ -10,7 +10,8 @@ public abstract class BMI {
     double mass;
     double height;
     public static String INVALID_ARG_EX_MESSAGE = "Invalid argument";
-    
+    private static double BMI_UNDERWEIGHT = 18.5;
+    private static double BMI_OVERWEIGHT = 25.0;
     public BMI(double mass, double height) {
         this.mass = mass;
         this.height = height;
@@ -20,8 +21,8 @@ public abstract class BMI {
     public int getColorInt() {
         double bmi = countBMI();
         if (bmi < 0) throw new IllegalArgumentException(BMI.INVALID_ARG_EX_MESSAGE);
-        if (bmi < 18.5) return Color.BLUE;
-        if (bmi >= 25.0) return Color.RED;
+        if (bmi < BMI_UNDERWEIGHT) return Color.BLUE;
+        if (bmi >= BMI_OVERWEIGHT) return Color.RED;
         return Color.GREEN;
     }
 
